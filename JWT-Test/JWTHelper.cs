f9cb086d157d2dc75bc59f74b7a6241d31cd08d2
tharindusandamal <tarindusandamal@gmail.com>
2018-12-18 18:23:57 +0530

    add new project

---
+++ JWT-Test/JWTHelper.cs
@@ -0,0 +1,49 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT_Test
+{
+    public class JWTHelper
+    {
+        private const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+
+        public string GetToken(object payload)
+        {
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            var token = encoder.Encode(payload, secret);
+            return token;
+        }
+
+        public string GetData(string token)
+        {
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
+
+                var json = decoder.Decode(token, secret, verify: true);
+                return json;
+            }
+            catch (TokenExpiredException ex)
+            {
+                throw new Exception(ex.InnerException.ToString());
+            }
+            catch (SignatureVerificationException ex)
+            {
+                throw new Exception(ex.InnerException.ToString());
+            }
+        }
+    }
+}
---
+++ JWT-Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWT_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JWTHelper jWTHelper = new JWTHelper();
+
+            var payload = new Dictionary<string, object>
+            {
+                { "claim1", 0 },
+                { "claim2", "claim2-value" }
+            };
+
+            var t = jWTHelper.GetToken(payload);
+
+            var d = jWTHelper.GetData(t);
+        }
+    }
+}
